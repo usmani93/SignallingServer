154e7a982beab0efaf74ca189194e19a6234b640
Mohammad Sami Usmani <sami.usmani48@gmail.com>
2023-02-19 17:46:56 +0500

    Use swagger in production

--- Program.cs
+++ Program.cs
@@ -19,11 +19,11 @@ namespace SignallingServer
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            //}
 
             app.UseHttpsRedirection();
 
